6e90933029b763d5ca48176352db71b58667693a
Darya <zahrakhalafy00@gmail.com>
2025-05-16 10:50:17 +0200

    validating input fields

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 //variables
-const sendBtn = document.querySelector("#sendBtn")
+const sendBtn = document.querySelector("#sendBtn"),
+      email = document.querySelector('#email'),
+      subject = document.querySelector('#subject'),
+      message = document.querySelector('#message')
 
 
 
@@ -8,12 +11,23 @@ eventListeners()
 function eventListeners() {
     //app initialization
     document.addEventListener("DOMContentLoaded", appInit)
+
+    //validating fields
+    email.addEventListener("blur", validateField)
+    subject.addEventListener("blur", validateField)
+    message.addEventListener("blur", validateField)
     
 }
 
 //functions
 function appInit() {
-
+    //disabled send button on load
     sendBtn.disabled = true
     
+}
+
+//validating field of form
+function validateField() {
+    // console.log("yes")
+     
 }
